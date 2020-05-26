a2f37f921e47ee88f28ab2e5e8e1fc6d1defd5b2
sick-tichun <tycjangaj@gmail.com>
2020-05-26 19:41:08 +0000

    added music commands

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
-import discord
-import asyncio
-import random
-import nekos
+import discord, asyncio, random, nekos, os, youtube_dl
 from discord.ext import commands, tasks
+from discord.utils import get
 from itertools import cycle
-import os
 import subprocess
-from textblob import TextBlob
-from textblob import Word
+from textblob import TextBlob, Word
 import scentence_creator
 import word_lsit_reader_writer
+import nltk
+nltk.download('wordnet')
+
 
 key = open("key.txt", "r").read()
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '£')
 
 @client.event
 async def on_ready():
@@ -24,11 +23,11 @@ async def on_ready():
 
 #when a member joins put text
 async def on_member_join(member):
-    member.send('have a happy stay :), fortbuck i hate download')
+    member.send('have a happy stay')
 
-#translates your phrase to damian text
 @client.command()
 async def dxmi(ctx, phrase):
+    "Translates your text into: thxs (remember to put speach marks around text if your writing a scentance)"
     trans = ''
     length = len(phrase)
     for i in range (0, length):
@@ -38,118 +37,112 @@ async def dxmi(ctx, phrase):
             trans = trans + phrase[i:i + 1]
     await ctx.send(trans)
 
-#just a dos attack
 @client.command()
 async def ddos(ctx, ip):
+    "pings an ip a couple times"
     os.system('timeout 3 ping ' + ip + ' -c 5')
     await ctx.send('pinged ' + ip + ' 5 times for 3 seconds')
 
-#spams a message the specified amount of times
 @client.command()
 async def spam(ctx, msg, nummy):
+    "spams a message the specified amount of times"
     for i in range(0, int(nummy)):
         await ctx.send(msg + ' ' + str(i + 1))
 
-#adds 2 numbers
 @client.command()
 async def scumcalc_add(ctx, num1, num2):
+    "adds 2 numbers"
     calc = float(num1) + float(num2)
     await ctx.send(calc)
 
-#multiplies 2 numbers
+
 @client.command()
 async def scumcalc_mult(ctx, num1, num2):
+    "multiplies 2 numbers"
     calc = float(num1)*float(num2)
     await ctx.send(calc)
 
-#divides 2 numbers
 @client.command()
 async def scumcalc_div(ctx, nummy1, nummy2):
+    "divides 2 numbers"
     calc = float(nummy1)/float(nummy2)
     await ctx.send('here youb go fathead :() ' + str(calc))
 
+@client.command()
+async def invite(ctx):
+    "invite link to the bot"
+    await ctx.send('https://discordapp.com/oauth2/authorize?&client_id=436175838363385866&scope=bot&permissions=8%27)/n/n/n@client.event/nasync')
+
+
 #neko.life api commands  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#sendds cat image
 @client.command()
 async def kitty(ctx):
+    "sendds cat image"
     await ctx.send(nekos.cat())
 
-#sends a fun fact
 @client.command()
 async def funfact(ctx):
+    "sends a fun fact"
     await ctx.send(nekos.fact())
 
-#sends a very thought provoking question
 @client.command()
 async def sxd(ctx):
+    "sends a very thought provoking question"
     await ctx.send(nekos.why())
 
-#sends an anime
-@client.command()
-async def neko(ctx):
-    await ctx.send(nekos.img('neko'))
-
-#sends anime, the amount of times specified
-@client.command()
-async def nekospam(ctx, num):
-    for i in range(1, int(num)):
-        await ctx.send(nekos.img('neko') + ' ' + i)
-
-#sends hentai... however many times you need, no nsfw check as its funny spamming this command everywhere
 @client.command()
 async def hentaispam(ctx, num):
+    "sends hentai... however many times you want, no nsfw check :()"
     for i in range(1, int(num)):
         await ctx.send(nekos.img('hentai') + ' ' + i)
 
-#sends hentai but also checks if its a nsfw enabled channel
 @client.command()
 async def hentai(ctx):
+    "sends hentai but also checks if its a nsfw enabled channel"
     if ctx.channel.is_nsfw()==True:
         await ctx.send(nekos.img('hentai'))
     else: 
         await ctx.send('sorry buddy but this isnt a nsfw channel')
 
-#sends a from any of the nekos.life api's categories
 @client.command()
 async def img(ctx, image):
+    "sends an image from any of the nekos.life api's categories"
     await ctx.send(nekos.img(str(image)))
+
 
 #tesxt blob api commands ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#translation command
 @client.command()
 async def trans(ctx, text, lang):
+    "translates your message (remember speach marks!!)"
     await ctx.send(TextBlob(text).translate(to=lang))
 
-#defenition command
-#@client.command()
-#async def meaning(ctx, defword):
-#    await ctx.send(Word(defword).define)
-
-
-#presence - user setting (may be removed as is practically uselsess)
 @client.command()
-async def playin(ctx, gamer):
-    await client.change_presence(activity=discord.Game(gamer))
-
+async def meaning(ctx, defword):
+    "defines a word"
+    defenition_w = Word(defword).definitions
+    await ctx.send(defenition_w)
 
 @client.command()
 async def sentence(ctx):
+    "creates a (usually) gramatically scentance"
     await ctx.send(scentence_creator.create())
 
 @client.command()
 async def addword(ctx, word_to_add):
+    "adds words to the scentance creation dictionary"
     word_lsit_reader_writer.word_add(word_to_add)
     await ctx.send('added ' + word_to_add + ' to the list of words')
 
 @client.command()
 async def readwords(ctx):
+    "sends the entire dictionary of words so fat but gets cut off"
     await ctx.send(word_lsit_reader_writer.word_read())
 
-#shell executes shell commands in the computer hosting the bot! - prevents usage of rm comand becase i dont want to deal with my files being deleted
 @client.command()
 async def shell(ctx, command):
+    "shell executes shell commands in the computer hosting the bot!"
     print (command[0:2])
     if command[0:2] == 'rm':
         await ctx.send('nanana fathead your not deleting anyyything from this computer')
@@ -158,10 +151,88 @@ async def shell(ctx, command):
         formatted_output = str(og_output).replace('\\n', '\n')[2:2000]
         await ctx.send(formatted_output[:-2])
 
-#sends invite link for the bot as i cant be bothered to make it myself anymore
+
+
+#Music Commands ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 @client.command()
-async def invite(ctx):
-    await ctx.send('https://discordapp.com/oauth2/authorize?&client_id=436175838363385866&scope=bot&permissions=8%27)/n/n/n@client.event/nasync')
+async def join(ctx):
+    "Joins a voice channel"
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+@client.command()
+async def dc(ctx):
+    "dcs from da voice"
+    await ctx.voice_client.disconnect()
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+@client.command()
+async def play_local(ctx, query):
+        "Plays a locally stored song"
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source)
+        await ctx.send('attempting to play: {}'.format(query))
+
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+@client.command()
+async def play(ctx, url):
+    "plays vid from yt!"
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, stream=True)
+        ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    await ctx.send('Now playing: {}'.format(player.title))
+
+
+#Misc ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #presence - automatic cylce
 status = cycle(['github.com/sick-tichun', 'with your mum(s)', 'cashin checks', 'hittin it out da glass', 'sub2 tichun on yt', 'doin ya mom', 'clownin and keepin it rock']) #list of all clycling names
@@ -169,5 +240,4 @@ status = cycle(['github.com/sick-tichun', 'with your mum(s)', 'cashin checks', '
 async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
 
-#the secret key!
 client.run(key)
